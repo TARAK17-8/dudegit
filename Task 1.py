682fd6cd5be7bfd1d33090c3a09886ffb319187e
TARAK17-8 <mohantarak1234@gmail.com>
2025-12-07 20:44:28 +0530

    Assignment 1

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+#Basic Mathematical Operations
+a=int(input("enter first number: "))
+b=int(input("enter second number: "))
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+if b == 0:
+    print("error!..can't divide with zero")
+else:
+    print("Division:", a/b)
